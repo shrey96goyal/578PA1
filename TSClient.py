69f07433141d964c75b73a1a91672a57be53c9bd
shrey96goyal <sgoyal@umass.edu>
2023-10-01 16:22:20 -0400

    changes

--- TSClient.py
+++ TSClient.py
@@ -3,35 +3,33 @@ import socket
 import sys
 
 host = "127.0.0.1"
-port = 14412
+port = 18442
 if len(sys.argv) >= 2:
     host = sys.argv[1]
 
-while True:
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    try:
-        soc.connect((host, port))
-    except:
-        print('no connection')
+try:
+    soc.connect((host, port))
+except:
+    print('no connection')
 
-    time1 = int(time.time()*1000)
-    soc.sendall("Sync".encode('utf8'))
+time1 = int(time.time()*1000)
+soc.sendall("Sync".encode('utf8'))
 
-    serverResponse = soc.recv(100).decode('utf8').rstrip()
-    time4 = int(time.time()*1000)
+serverResponse = soc.recv(100).decode('utf8').rstrip()
+time4 = int(time.time()*1000)
 
-    serverTime2, serverTime3 = serverResponse.split(',')
-    serverTime2 = int(serverTime2)
-    serverTime3 = int(serverTime3)
+serverTime2, serverTime3 = serverResponse.split(',')
+serverTime2 = int(serverTime2)
+serverTime3 = int(serverTime3)
 
-    diff = (serverTime2 - time1) + (serverTime3 - time4)
-    diff /= 2
+diff = (serverTime2 - time1) + (serverTime3 - time4)
+diff /= 2
 
-    curTime = time4 + diff
+syncedTime = time4 + diff
 
-    rtt = (time4 - time1) + (serverTime3 - serverTime2)
-    print('REMOTE_TIME ' + str(int(curTime)))
-    print('LOCAL_TIME ' + str(int(time4)))
-    print('RTT_ESTIMATE ' + str(int(rtt)))
-    time.sleep(5)
+rtt = (time4 - time1) + (serverTime3 - serverTime2)
+print('REMOTE_TIME ' + str(int(syncedTime)))
+print('LOCAL_TIME ' + str(int(time4)))
+print('RTT_ESTIMATE ' + str(int(rtt)))
--- TSServer.py
+++ TSServer.py
@@ -4,31 +4,26 @@ import threading
 import time
 
 def func(connection, ip, port):
-    print('hi')
-    _ = connection.recv(16).decode('utf8').rstrip()
-    serverTime2 = int(time.time()*1000)
-    print('Client IP:' + str(ip) + ' Port: ' + str(port))
-    serverTime3 = int(time.time()*1000)
-    message = str(serverTime2) + ',' + str(serverTime3)
-    print(serverTime2)
 
-    print(serverTime3)
-    connection.sendall(message.encode('utf8'))
+    recvMessage = connection.recv(16).decode('utf8').rstrip()
+    if recvMessage == "Sync":
+        serverTime2 = int(time.time()*1000)
+        print('Client IP:' + str(ip) + ' Port: ' + str(port))
+        serverTime3 = int(time.time()*1000)
+        message = str(serverTime2) + ',' + str(serverTime3)
+        connection.sendall(message.encode('utf8'))
     connection.close()
 
 host = ""
-port = 14412
+port = 18442
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 
-# put the socket into listening mode
 s.listen()
+print("Server running")
 
-# a forever loop until client wants to exit
 try:
     while True:
-
-        # establish connection with client
         connection, addr = s.accept()
         ip, port = str(addr[0]), str(addr[1])
         try:
